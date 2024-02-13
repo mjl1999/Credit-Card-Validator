e9ed2ecc09c9a8a13a1b5b21913dd339197bf2cc
mjl1999 <40575814+mjl1999@users.noreply.github.com>
2024-02-13 22:43:12 +0000

    Add files via upload

---
+++ revised credit card checker.py
@@ -0,0 +1,58 @@
+def main():
+    card = input("Please enter your 16 digit card number (no spaces or hyphens needed): ").replace(" ", "").replace("-", "")
+    card = check_card(card)
+    validate(card)
+
+def validate(card):
+    r_card = card[::-1]
+    total_of_n1 = sum_n1(r_card)
+    total_of_n2 = sum_n2(r_card)
+    total = total_of_n1 + total_of_n2
+    if total % 10 == 0:
+        print("Valid card")
+    else:
+        print("Invalid card")
+
+def sum_n1(card):
+    total = 0
+    for num in card[::2]:
+        total += int(num)
+    return total
+
+
+def sum_n2(card):
+    total = 0
+    for num in card[1::2]:
+        num = int(num) * 2
+        if num >= 10:
+            num = (num // 10) + (num % 10)
+        total += num
+    return total
+
+def check_card(card):
+    retry_attempts = 3
+    while retry_attempts != 0:
+        if check_digits(card) and check_length(card):
+            return card
+        else:
+            card = input(f"Warning {retry_attempts} retries remaining!!! Please re-enter your card details:\n")
+            retry_attempts -= 1
+    print("Retry attempts exceeded. Your portal has been locked for 24 hours")
+    quit()
+
+def check_digits(card):
+    try:
+        int(card)
+        return True
+    except ValueError:
+        print("Please enter digits only.")
+        return False
+
+def check_length(card):
+    if len(card) == 16:
+        return True
+    else:
+        print("Your card should only be 16 digits long")
+        return False
+
+main()
